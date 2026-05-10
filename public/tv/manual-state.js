04f825645c5b8d29a75eff6a5336a5dc487ef852
Bossenomad <bosc016@hotmail.com>
2026-05-10 14:30:42 +0200

    Update TV: set Sigge Svensson score to 52

--- public/tv/manual-state.js
+++ public/tv/manual-state.js
@@ -1,10 +1,10 @@
 window.MANUAL_TV_STATE = {
   prizePot: 7351,
   leaderboard: [
-    { playerId: 'p1', displayName: 'Morris S', favouriteClub: 'Karlskrona HC', score: 47 },
-    { playerId: 'p2', displayName: 'Samuel B', favouriteClub: 'IFK Tumba', score: 44 },
-    { playerId: 'p3', displayName: 'Rasmus Pilebo', favouriteClub: 'Helsingborg HC', score: 42 },
-    { playerId: 'p4', displayName: 'Sigge Svensson', favouriteClub: 'Karlskrona HC', score: 42 },
+    { playerId: 'p1', displayName: 'Sigge Svensson', favouriteClub: 'Karlskrona HC', score: 52 },
+    { playerId: 'p2', displayName: 'Morris S', favouriteClub: 'Karlskrona HC', score: 47 },
+    { playerId: 'p3', displayName: 'Samuel B', favouriteClub: 'IFK Tumba', score: 44 },
+    { playerId: 'p4', displayName: 'Rasmus Pilebo', favouriteClub: 'Helsingborg HC', score: 42 },
     { playerId: 'p5', displayName: 'Joakim', favouriteClub: 'Bollnäs IS', score: 40 },
   ],
   latestResult: {
